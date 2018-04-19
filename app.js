d5eb7e01a6075d72f10f9c8d23469d8354a92ec5
pranavSI <pranavSI@users.noreply.github.com>
2018-04-20 00:39:29 +0530

    Setup Environment Variables

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 
 // Imports dependencies and set up http server
 const
+    config = require('config'),
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 5000, () => console.log('I\'m here. I\'m waiting.'));
+
+// Arbitrary value used to validate a webhook
+const
+    VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+    (process.env.MESSENGER_VALIDATION_TOKEN) :
+    config.get('validation_token');
+
+// App Secret can be retrieved from the App Dashboard
+const
+    APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+    process.env.MESSENGER_APP_SECRET :
+    config.get('app_secret');
+
+// Generate a page access token for your page from the App Dashboard
+const
+    PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+    config.get('page_access_token');
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
@@ -38,9 +57,6 @@ app.post('/webhook', (req, res) => {
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "KXIPFBBOT2018"
-
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -50,7 +66,7 @@ app.get('/webhook', (req, res) => {
     if (mode && token) {
 
         // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        if (mode === 'subscribe' && token === VALIDATION_TOKEN) {
 
             // Responds with the challenge token from the request
             console.log('WEBHOOK_VERIFIED');
